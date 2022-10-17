dde768730fe7a00de5580504ed68f27dece72e65
KostyaHonchar <108327242+KostyaHonchar@users.noreply.github.com>
2022-10-17 21:00:03 +0300

    Tasks from Volodya

---
+++ src/components/common/header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Navbar } from '../../common';
+
+import './Header.css';
+
+function Header() {
+   return (
+      <section >
+         <section className='header'>
+            <a href="/" className='header-logo'>GIRKO</a>
+            <a href="/photographs" className='header-lists'>Фотографи</a>
+            <a href="/videomakers" className='header-lists'>Відеооператори</a>
+            <a href="/musicians" className='header-lists'>Музиканти</a>
+            <a href="/musicians" className='header-lists'>Ведучі</a>
+            <a href="/musicians" className='header-lists'>Ресторани</a>
+            <a href="/login" className='header-login'>ВХІД</a>
+            <a href="/register" className='header-registration'>РЕЄСТРАЦІЯ</a>
+         </section>
+      </section>
+)}
+
+export default Header;
