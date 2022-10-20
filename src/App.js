7bf5920f7d325c9e922960185a227aa8f80eb37f
KostyaHonchar <108327242+KostyaHonchar@users.noreply.github.com>
2022-10-20 12:05:43 +0300

    Cleaning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from "react";
-import { Header } from "./components/common";
+import { Header } from ".components/common";
 
 import "./App.css";
-
 function App() {
   return (
     <div className="App">
