056ef6e502d1c50692040538e5a66a1ab7168386
KostyaHonchar <108327242+KostyaHonchar@users.noreply.github.com>
2022-10-27 14:37:18 +0300

    Few updates

--- src/App.js
+++ src/App.js
@@ -1,42 +1,30 @@
 import React from "react";
-import "./App.css";
 import ShowcaseImage from "./components/files/showcase.jpeg";
 import Header from "./components/header/Header";
-import Showcase from "./components/Combobox";
-import Highlights from "./components/Combobox2";
+import Showcase from "./components/SearchBox/Combobox";
+import Highlights from "./components/SearchBox/Combobox2";
 import AutoGrid from "./components/Category Grid";
-import { Container, Typography } from "@mui/material";
-import { SxProps, Theme } from "@mui/material/styles";
+import {
+  Container,
+  Typography,
+  CssBaseline,
+  ThemeProvider,
+} from "@mui/material";
+
+import { palette } from "@mui/system";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Container fixed>
-        <Showcase />
-        <Highlights sx={{ boxShadow: 1 }} />
-        <img
-          src={ShowcaseImage}
-          alt="Logo"
-          className="showcase-photo"
-          sx={{ zIndex: "modal" }}
-        ></img>
-      </Container>
-      <Typography
-        sx={{
-          color: "#4f5259;",
-          display: "inline",
-          fontWeight: "bold",
-          my: 100,
-          fontSize: 40,
-          textAlign: "center",
-        }}
-      >
-        <h1>Каталог весільних послуг</h1>
-        <h4>12011 професіоналів із 24 міст України.</h4>
-        <h4> Плануйте весілля разом з нами.</h4>
-      </Typography>
-      <AutoGrid />
+      <Showcase />
+      <Highlights sx={{ boxShadow: 1 }} />
+      <img
+        src={ShowcaseImage}
+        alt="Logo"
+        className="showcase-photo"
+        width="500px"
+      />
     </div>
   );
 }
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,25 @@
+import { Box } from '@mui/material';
 import React from 'react';
-import './Header.css';
+import { palette } from '@mui/system';
+
+
 
 function Header() {
    return (
-      <section >
-         <section className='header'>
+      <Box>
+         <Box sx={{ display: 'flex', justifyContent: "flex-start", fontSize: '14px', p: '11px', bgcolor: '#fff' ,border: '1px solid black'}}>
             <a href="/" className='header-logo'>GIRKO</a>
             <a href="/photographs" className='header-lists'>Фотографи</a>
             <a href="/videomakers" className='header-lists'>Відеооператори</a>
             <a href="/musicians" className='header-lists'>Музиканти</a>
             <a href="/musicians" className='header-lists'>Ведучі</a>
             <a href="/musicians" className='header-lists'>Ресторани</a>
+         </Box>
+         <Box sx={{ display: 'flex', justifyContent: "flex-end", fontSize: '14px', p: '10px', bgcolor: '#fff', border: '1px solid black',pb:'-10px',zIndex:'-20'}}>
             <a href="/login" className='header-login'>ВХІД</a>
             <a href="/register" className='header-registration'>РЕЄСТРАЦІЯ</a>
-         </section>
-      </section>
+         </Box>
+      </Box>
    )
 }
 
