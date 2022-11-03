03727105d1914062dd2b13cc6e20039774c07951
Volodymyr Yatsyk <volodymyr@buywith.com>
2022-11-03 21:07:23 +0200

    Combobox single component.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React from "react";
 import "./index.css";
 import Header from "./components/header/Header";
-import Showcase from "./components/SearchBox/Combobox";
-import Highlights from "./components/SearchBox/Combobox2";
 import CategoryGrid from "./components/Category Grid";
+import SearchBox from './components/SearchBox';
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <div className="searchbox">
-        <Showcase className="showcase" />
-        <Highlights className="highlights" />
-      </div>
+      <SearchBox />
       <div className="text-middle">
         <h1>Каталог весільних послуг</h1>
         <h4>12013 професіоналів із 24 міст України.</h4>
