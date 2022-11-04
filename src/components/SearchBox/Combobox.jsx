691331fe47500f9bcff337544d4d777015bf1323
KostyaHonchar <108327242+KostyaHonchar@users.noreply.github.com>
2022-11-04 11:32:18 +0200

    download form git

--- src/components/SearchBox/Combobox.jsx
+++ src/components/SearchBox/Combobox.jsx
@@ -2,45 +2,18 @@ import * as React from 'react';
 import TextField from '@mui/material/TextField';
 import Autocomplete from '@mui/material/Autocomplete';
 
-
-
-const categoryWithOptions = () => [
-  { label: 'Фотографи' },
-  { label: 'Відеооператори' },
-  { label: 'Ведучі' },
-  { label: 'Ресторани' },
-  { label: 'Оформлення залу' },
-  { label: 'Сукні для дружок' },
-  { label: 'Весільні сукні' },
-  { label: 'Торти та короваї' },
-  { label: 'Автомобілі' },
-  { label: 'Організація весілля' },
-  { label: 'Шоу-програма' },
-  { label: 'Салони краси' },
-  { label: 'Запрошення та аксесуари' },
-  { label: 'Перший танець' },
-  { label: 'Фуршетний стіл' },
-  { label: 'Весільні букети' },
-
-
-];
-
-
-
-
-export default function ComboBox() {
+export default function ComboBox({ title, data, label }) {
   return (
     <div className='searchbox-box'>
-      <span className='span-searchbox'>Категорія
+      <span className='span-searchbox'>{title}</span>
         <Autocomplete
           className='combo-box-demo-class'
           disablePortal
-          id="combo-box-demo"
-          options={categoryWithOptions()}
+          options={data}
+          size="small"
           sx={{ width: 250, height: "100%", bgcolor: "#fff", borderRadius: "3px ", color: "#999", mt: "10px" }}
-          renderInput={(params) => <TextField sx={{}} {...params} label="Оберіть категорію" />}
+          renderInput={(params) => <TextField {...params} label={label} />}
         />
-      </span>
     </div>
   );
 }
