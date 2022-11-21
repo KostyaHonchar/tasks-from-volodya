ea5037dd4a9bb4885d6352f334d51f70b2f32914
KostyaHonchar <108327242+KostyaHonchar@users.noreply.github.com>
2022-11-21 19:28:48 +0200

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Header from "./components/header/Header";
 import CategoryGrid from "./components/Category Grid";
 import SearchBox from "./components/SearchBox";
 import TextMiddle from "./components/Text-middle";
+import Clearfix from "./components/Clearfix";
+
 
 export default function App() {
   return (
@@ -12,6 +14,7 @@ export default function App() {
       <SearchBox />
       <TextMiddle />
       <CategoryGrid />
+      <Clearfix />
     </div>
   );
 }
---
+++ src/components/Clearfix.jsx
@@ -0,0 +1,79 @@
+/* eslint-disable jsx-a11y/heading-has-content */
+import React from "react";
+
+export default function Clearfix() {
+   return (
+      <div className="city-blocks">
+         <div className="clearfix">
+            <a href="/#" className="city-block-item">
+               <div className="overlay"></div>
+               <div className="info">
+                  <h2 className="title">Київ</h2>
+                  <div className="description">
+                     1567 професіоналів
+                     <br />
+                     для вашого весілля
+                  </div>
+               </div>
+            </a>
+            <a href="/#" className="city-block-item">
+               <div className="overlay"></div>
+               <div className="info">
+                  <h2 className="title">Львів</h2>
+                  <div className="description">
+                     4751 професіоналів
+                     <br />
+                     для вашого весілля
+                  </div>
+               </div>
+            </a>
+            <a href="/#" className="city-block-item">
+               <div className="overlay"></div>
+               <div className="info">
+                  <h2 className="title">Івано-Франківськ</h2>
+                  <div className="description">
+                     1364 професіоналів
+                     <br />
+                     для вашого весілля
+                  </div>
+               </div>
+            </a>
+            <a href="/#" className="city-block-item">
+               <div className="overlay"></div>
+               <div className="info">
+                  <h2 className="title">Тернопіль</h2>
+                  <div className="description">
+                     1042 професіоналів
+                     <br />
+                     для вашого весілля
+                  </div>
+               </div>
+            </a>
+            <a href="/#" className="city-block-item">
+               <div className="overlay"></div>
+               <div className="info">
+                  <h2 className="title">Луцьк</h2>
+                  <div className="description">
+                     690 професіоналів
+                     <br />
+                     для вашого весілля
+                  </div>
+               </div>
+            </a>
+            <a href="/#" className="city-block-item">
+               <div className="overlay"></div>
+               <div className="info">
+                  <h2 className="title">Харків</h2>
+                  <div className="description">
+                     277 професіоналів
+                     <br />
+                     для вашого весілля
+                  </div>
+               </div>
+            </a>
+         </div>
+      </div>
+
+
+   )
+}
